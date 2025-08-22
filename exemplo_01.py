17d3e68c46f661415611e38f7bb52908e4b64deb
Carlos Roberto Ferreira Alves <carloscorretor15@gmail.com>
2025-08-22 00:13:12 -0300

    Adição do banco de dados

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,4 +1,5 @@
 import requests
+from tinydb import TinyDB
 
 def extrair():
     url = "https://api.coinbase.com/v2/prices/spot"
@@ -6,6 +7,8 @@ def extrair():
     return response.json()
 
 def transformar(dados):
+    
+  
     valor = dados['data']['amount']
     criptomoeda = dados['data']['base']
     moeda = dados['data']['currency']
@@ -17,7 +20,17 @@ def transformar(dados):
       }
     return dados_tratados
 
+def load(dados_tratados):
+    db = TinyDB('db.json')
+    db.insert(dados_tratados)
+    print("Dados salvos com sucesso!")
+
+
 if __name__ == "__main__":
     dados_extraidos = extrair()
     tratados = transformar(dados_extraidos)
-    print(tratados)
+    
+    load(tratados)
+    
+    
+    
