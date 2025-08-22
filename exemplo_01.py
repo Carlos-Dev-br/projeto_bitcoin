107fc3a9aaa5c850410525bd8d992edbd0d848b5
Carlos Roberto Ferreira Alves <carloscorretor15@gmail.com>
2025-08-21 23:39:00 -0300

    Incluindo função

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,9 +1,23 @@
 import requests
 
-url = "https://api.coinbase.com/v2/prices/spot"
+def extrair():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()
 
-response = requests.get(url)
+def transformar(dados):
+    valor = dados['data']['amount']
+    criptomoeda = dados['data']['base']
+    moeda = dados['data']['currency']
+    
+    dados_tratados = { 
+      "valor": valor, 
+      "criptomoeda": criptomoeda,
+      "moeda": moeda 
+      }
+    return dados_tratados
 
-print(response.json())
-
-
+if __name__ == "__main__":
+    dados_extraidos = extrair()
+    tratados = transformar(dados_extraidos)
+    print(tratados)
